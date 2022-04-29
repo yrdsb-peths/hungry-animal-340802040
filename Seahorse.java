eab923c0bb681523bcfd35076af1d73c7959a7d8
340802040 <340802040@gapps.yrdsb.ca>
2022-04-29 13:32:00 -0400

    l

---
+++ Seahorse.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Seahorse extends Actor
+{
+
+    public void act()
+    {
+        move(1);
+    }
+}
