360464e2a044165f022c7f3f38cb301a46b1e62c
340802040 <340802040@gapps.yrdsb.ca>
2022-05-25 15:00:25 -0400

    ver 3

--- Seahorse.java
+++ Seahorse.java
@@ -2,7 +2,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Seahorse extends Actor
 {
-
+    SimpleTimer animationTimer = new SimpleTimer();
+    GreenfootImage[] idle = new GreenfootImage[6];
+    
+    public Seahorse()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            idle[i] = new GreenfootImage("images/tile00" + i + ".png");
+            idle[i].scale(40, 60);
+        }
+        setImage(idle[0]);
+        
+        animationTimer.mark();
+    }
+    
+    int imageIndex = 0;
+    public void animateSeahorse()
+    {
+        if(animationTimer.millisElapsed() < 90)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("a"))
@@ -24,6 +51,7 @@ public class Seahorse extends Actor
         
         eat();
         
+        animateSeahorse();
     }
     
     public void eat()
